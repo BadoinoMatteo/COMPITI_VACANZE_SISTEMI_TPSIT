3acd141ee948f412e574bab8551c764a7dd1ff59
Badoino Matteo <48619446+BadoinoMatteo@users.noreply.github.com>
2020-09-01 11:33:06 +0200

    compito tpsit

--- TPSIT/server.py
+++ TPSIT/server.py
@@ -1,33 +1,37 @@
 import socket
 import threading
 
+localIP = "127.0.0.1"
+localPort = 80
+lock = threading.Lock()
+mex = ""
+listaClient = []
 
-localIP= "127.0.0.1"
-localPort= 80
-lock=threading.Lock()
-mex=""
-listaClient=[]
 
 def main():
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listaClient = []
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((localIP, localPort))
 
     s.listen()
-    conn, addr = s.accept()
     while True:
-        mex=conn.recv(4096)
-        thred = (threading.Thread(target=client(), args=mex))
-        listaClient.append(thred)
-        listaClient.start()
+        conn, addr = s.accept()
+        #mex = conn.recv(4096)
+        thread=threading.Thread(target=client, args=(conn, addr))
+        thread.start()
+        listaClient.append(thread)
 
-    for i, t in enumerate (listaClient):
+    for i, t in enumerate(listaClient):
         t.join()
+
     s.close()
 
-def client():
-    lock.acquire()
+
+def client(conn, addr):
+    mex=conn.recv(4096).decode()
     print(mex)
-    lock.release()
+
+
 
 if __name__ == '__main__':
     main()
